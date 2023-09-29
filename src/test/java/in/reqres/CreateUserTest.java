043053f8c160935ace6fdaeefb3f7ee6a45cd8e7
ameryakubov <alex_meryakubov@mail.ru>
2023-09-29 22:12:55 +0300

    first commit

---
+++ src/test/java/in/reqres/CreateUserTest.java
@@ -0,0 +1,77 @@
+package in.reqres;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.is;
+
+public class CreateUserTest {
+    @Test
+    void successCreateUserTest() {
+        String createData = "{\"name\": \"morpheus\",\"job\": \"leader\"}";
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(createData)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"))
+                .body("id", is(not(empty())))
+                .body("createdAt", is(not(empty())));
+
+    }
+
+    @Test
+    void createUserWithoutNameTest() {
+        String createData = "{\"name\": \"\",\"job\": \"leader\"}";
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(createData)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("job", is("leader"))
+                .body("id", is(not(empty())))
+                .body("createdAt", is(not(empty())));
+
+    }
+
+    @Test
+    void createUserWithoutJobTest() {
+        String noneJobData = "{ \"name\": \"morpheus\" }";
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(noneJobData)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"),
+                        "id", is(not(empty())),
+                        "createdAt", is(not(empty())));
+    }
+}
---
+++ src/test/java/in/reqres/UpdateUserTest.java
@@ -0,0 +1,52 @@
+package in.reqres;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.empty;
+
+public class UpdateUserTest {
+    @Test
+    void successUpdateUserTest() {
+        String createData = "{\"name\": \"morpheus\",\"job\": \"zion resident\"}";
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(createData)
+                .when()
+                .post("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("zion resident"))
+                .body("createdAt", is(not(empty())));
+    }
+
+    @Test
+    void updateUserWithPatchMethodTest() {
+        String createData = "{\"name\": \"Dark\",\"job\": \"zion resident\"}";
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(createData)
+                .when()
+                .patch("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("name", is("Dark"))
+                .body("job", is("zion resident"))
+                .body("createdAt", is(not(empty())));
+    }
+}
